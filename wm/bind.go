5e71be1c827b6c98cd145b99cbcdb77d4ce9ca0f
Jake Teton-Landis <just.1.jake@gmail.com>
2013-05-24 03:10:29 -0700

    window management and state tracking

---
+++ wm/bind.go
@@ -0,0 +1,143 @@
+package wm
+
+/*
+bind.go handles action binding and state management for j3
+The job of this file is to
+    * note if a drag is occuring
+    * track the current incoming (drag-start) window
+    * deduce the current target (moused-over) window
+    * move the cross over the target
+    * handle drop-actions on the cross's window interaction icons
+*/
+
+import (
+    "github.com/BurntSushi/xgb/xproto"
+    "github.com/BurntSushi/xgbutil"
+    "github.com/BurntSushi/xgbutil/xwindow"
+    "github.com/BurntSushi/xgbutil/ewmh"
+
+    "github.com/justjake/j3/ui"
+    "errors"
+    "fmt"
+)
+    
+
+// we will eventually build the cross out of DropZones instead of bare icons
+// DropZones link the presentaion element (the icon) with a window action
+type DropZone struct {
+    Icon      *ui.Icon 
+    Action    WindowInteraction
+}
+// we can get the window under the mouse from X11
+// we use that information to find the DropZone, and thus the action,
+// from the icon's window
+var windowToDropZone map[xproto.Window]*DropZone = make(map[xproto.Window]*DropZone, 10)
+
+func NewDropZone(icn *ui.Icon, action WindowInteraction) *DropZone {
+    dz := DropZone{icn, action}
+    id := icn.Window.Id
+    windowToDropZone[id] = &dz
+    return &dz
+}
+
+// find the window directly under the mosue cursor at this time
+// synchronous
+// TODO: reconsider using QueryPointer directly for dat async performance
+func FindUnderMouse(X *xgbutil.XUtil) (xproto.Window, error) {
+    // start query pointer request
+    cookie := xproto.QueryPointer(X.Conn(), X.RootWin())
+
+    // construct a hashset of the managed windows
+    clients, err := ewmh.ClientListGet(X)
+    if err != nil {
+        return 0, fmt.Errorf("FindUnderMouse: could not retrieve EWHM client list: %v", err)
+    }
+    managed := make(map[xproto.Window]bool, len(clients))
+    for _, win := range clients {
+        managed[win] = true
+    }
+
+    // block and get reply for client
+    reply, err := cookie.Reply()
+    if err != nil {
+        return 0, err
+    }
+    child := reply.Child
+    root  := X.RootWin()
+
+    for child != root {
+        // test to see if child is a managed window
+        _, ok := managed[child]
+        if ok {
+            return child, nil
+        }
+
+        // traverse upwards
+        tree, err := xproto.QueryTree(X.Conn(), child).Reply()
+        if err != nil {
+            return 0, fmt.Errorf("FindUnderMouse: tree traversal error: %v", err)
+        }
+        child = tree.Parent
+    }
+    // we didn't find the window :(
+    return 0, fmt.Errorf("FindUnderMouse: window under mouse %i not found in EWMH clients %v", reply.Child, clients)
+}
+
+// meat and potatoes of our window manager
+// tracks drags, invokes drops
+type DragManager struct {
+    Dragging bool
+    Target   *xwindow.Window
+    Incoming *xwindow.Window
+
+    // actuals
+    cross    *xwindow.Window
+    x        *xgbutil.XUtil
+}
+
+
+func NewDragManager(X *xgbutil.XUtil, cross *xwindow.Window) *DragManager {
+    dm := DragManager{
+        Dragging: false,
+        Target: nil,
+        Incoming: nil,
+        cross: cross,
+        x: X,
+    }
+    return &dm
+}
+    
+
+func (dm *DragManager) StartDrag(incoming *xwindow.Window) {
+    dm.Dragging = true
+    dm.Incoming = incoming
+
+    // TODO: listen harder?
+}
+
+func (dm *DragManager) SetTarget(target *xwindow.Window) {
+    dm.Target = target
+}
+
+// this needs significant reconsideration
+func (dm *DragManager) EndDrag(dz *DropZone) error {
+    // end the drag no matter what
+    t, i := dm.Target, dm.Incoming
+    dm.Target, dm.Incoming = nil, nil
+
+    if !dm.Dragging {
+        return errors.New("Cannot end drag: not currently dragging")
+    }
+    dm.Dragging = false
+
+    if i == nil {
+        return errors.New("Cannot end drag: no incoming window")
+    }
+
+    if t == nil {
+        return errors.New("Cannot end drag: no target window")
+    }
+
+    // invoke the drag zone action
+    return dz.Action(t, i)
+}
---
+++ wm/move.go
@@ -0,0 +1,150 @@
+package wm
+
+/* Move.go
+   handles moving windows about using EWMH interaction commands
+   I think we can usually just use xwindow.Window objects for convinience
+   */
+import (
+    "github.com/BurntSushi/xgbutil/xwindow"
+
+    "log"
+)
+
+type WindowInteraction func(*xwindow.Window, *xwindow.Window) (error)
+
+// Split functions:
+// split the target window's area in half. The incoming window takes the split
+// half in the direction of the function name. So in SpitTop, the target is the
+// bottom half of the initial area, and the incoming is the top.
+
+// cutting windows in half on the X axis
+func splitVertical(target, incoming *xwindow.Window, incomingOnTop bool) error {
+    bounds, err := target.DecorGeometry()
+    if err != nil {
+        log.Printf("splitVertical: error getting bounds of target: %v\n", err)
+        return err
+    }
+
+    bottom_height := bounds.Height() / 2
+    top_height := bounds.Height() - bottom_height
+
+    var top, bottom *xwindow.Window
+    if incomingOnTop {
+        top = incoming
+        bottom = target
+    } else {
+        top = target
+        bottom = incoming
+    }
+
+    // target goes on bottom...
+    err = bottom.WMMoveResize(bounds.X(), bounds.Y() + top_height, bounds.Width(), bottom_height)
+    if err != nil {
+        log.Printf("splitVertical: error configuring bottom: %v\n", err)
+        return err
+    }
+
+    // and incoming on top
+    err = top.WMMoveResize(bounds.X(), bounds.Y(), bounds.Width(), top_height)
+    if err != nil {
+        log.Printf("splitVertical: error configuring top: %v\n", err)
+        return err
+    }
+
+    // cool
+    return nil
+}
+
+// cutting windows in half on the Y axis
+func splitHorizontal(target, incoming *xwindow.Window, incomingOnLeft bool) error {
+    bounds, err := target.DecorGeometry()
+    if err != nil {
+        log.Printf("splitHorizontal: error getting bounds of target: %v\n", err)
+        return err
+    }
+
+    left_width := bounds.Width() / 2
+    right_width := bounds.Width() - left_width
+
+    var left, right *xwindow.Window
+    if incomingOnLeft {
+        left = incoming
+        right = target
+    } else {
+        left = target
+        right = incoming
+    }
+
+    err = right.WMMoveResize(bounds.X() + left_width, bounds.Y(), right_width, bounds.Height())
+    if err != nil {
+        log.Printf("splitHorizontal: error configuring right: %v\n", err)
+        return err
+    }
+
+    err = left.WMMoveResize(bounds.X(), bounds.Y(), left_width, bounds.Height())
+    if err != nil {
+        log.Printf("splitHorizontal: error configuring left: %v\n", err)
+        return err
+    }
+
+    // cool
+    return nil
+}
+
+// Exported split actions
+
+// Split the target window, putting the incoming window in the top half
+func SplitTop(target, incoming *xwindow.Window) error {
+    return splitVertical(target, incoming, true)
+}
+// Split the target window, putting the incoming window in the bottom half
+func SplitBottom(target, incoming *xwindow.Window) error {
+    return splitVertical(target, incoming, false)
+}
+// Split the target window, putting the incoming window in the left half
+func SplitLeft(target, incoming *xwindow.Window) error {
+    return splitHorizontal(target, incoming, true)
+}
+// Split the target window, putting the incoming window in the right half
+func SplitRight(target, incoming *xwindow.Window) error {
+    return splitHorizontal(target, incoming, false)
+}
+
+// Swap the position and size of the target and incoming windows
+func Swap(target, incoming *xwindow.Window) error {
+    // get bounds for both windows
+    target_bounds, err := target.DecorGeometry()
+    if err != nil {
+        log.Printf("Swap: error getting bounds of target: %v\n", err)
+        return err
+    }
+    incoming_bounds, err := incoming.DecorGeometry()
+    if err != nil {
+        log.Printf("Swap: error getting bounds of incoming: %v\n", err)
+        return err
+    }
+
+    // configure windows, easy as pie!
+    err = target.WMMoveResize(incoming_bounds.Pieces())
+    if err != nil {
+        log.Printf("Swap: error configuring target: %v\n", err)
+        return err
+    }
+    err = incoming.WMMoveResize(target_bounds.Pieces())
+    if err != nil {
+        log.Printf("Swap: error configuring incoming: %v\n", err)
+        return err
+    }
+
+    // cool
+    return nil
+}
+
+
+// TODO: shove actions
+// Shoves are basically just splits, but performed one-level-up, on a window's parent
+// THe current splits implementation works only for floating window managers,
+// which don't have crazy-cray nesting stuff
+// so shoves don't yet have any meaning.
+
+
