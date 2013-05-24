51499a7160de5d05cd0f37f7b87a582f490b24e8
Jake Teton-Landis <just.1.jake@gmail.com>
2013-05-23 22:02:42 -0700

    split icon into seperate file and added shaded states

--- ui/window.go
+++ ui/window.go
@@ -6,14 +6,10 @@ package ui
 import (
     "github.com/BurntSushi/xgb/xproto"
 
-    _ "github.com/BurntSushi/xgbutil/xevent"
     "github.com/BurntSushi/xgbutil/mousebind"
     "github.com/BurntSushi/xgbutil/xwindow"
-    "github.com/BurntSushi/xgbutil/xgraphics"
     "github.com/BurntSushi/xgbutil"
 
-    "image"
-    "image/color"
     "log"
 )
 
@@ -24,58 +20,7 @@ const (
     Mod = KeySuper + "-Shift"
 )
 
-// create a correctly-sized window from an image
-// almost not worth a function
-// TODO: transparency
 
-type Icon struct {
-    Image   image.Image         // original icon, from PNG usually
-    Parent  xproto.Window       // parent X window of the icon
-    Window  *xwindow.Window     // window that the icon is drawn to
-    ximage  *xgraphics.Image    // X11 image swap buffer, used to paint Image to Window
-}
-
-// get the pixels directly behind the icon
-// right now just returns an image.Uniform hard-coded to the right shade of grey
-// TODO actually do this
-func (icn *Icon) getBackground() image.Image {
-    log.Println("TODO: Icon.Blend()")
-    bg_color := color.RGBA{0xcc, 0xcc, 0xcc, 0xff}
-    bg := image.NewUniform(bg_color)
-    return bg
-}
-
-// blend the RGBA original icon against the background of Icon.Parent
-func (icn *Icon) Blend() {
-    // get pixels "parent-pixels" of Parent that are behind Window
-    bg := icn.getBackground()
-
-    // copy "parent-pixels" into buffer "ximage", overwriting existing completely
-    xgraphics.Blend(icn.ximage, bg, image.Point{0,0})
-
-    // alpha-blend Image into buffer "ximage"
-    xgraphics.Blend(icn.ximage, icn.Image, image.Point{0,0})
-
-    // swap ximage into Window as background
-    icn.ximage.XSurfaceSet(icn.Window.Id)
-    icn.ximage.XDraw()
-    icn.ximage.XPaint(icn.Window.Id)
-
-    // free the pixbuff memory!
-    icn.ximage.Destroy()
-}
-
-func (icn *Icon) Move(x, y int) {
-    icn.Window.Move(x, y)
-    icn.Blend()
-}
-
-func NewIcon(X *xgbutil.XUtil, img image.Image, parent xproto.Window) *Icon {
-    ximg := xgraphics.NewConvert(X, img)
-    win := ximg.Window(parent)
-    icn := Icon{img, parent, win, ximg}
-    return &icn
-}
 
 // drag around windows with the mouse.
 func MakeDraggable(X *xgbutil.XUtil, win xproto.Window) {
